071409111213fbaa65ccdeb87a94d0c798ec806f
Vladyslav <vladorangeqwer@gmail.com>
2024-11-28 19:10:21 -0500

    user already exists functionality
    fixed database name

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3001', // Frontend address
     methods: ['GET', 'POST', 'DELETE'],
   },
 });
